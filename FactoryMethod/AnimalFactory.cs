da91fada2eb3870ab1c25a9838637ee2dea559cc
John Ubaque <jubaque@sb.gob.do>
2024-03-13 08:52:43 -0500

    Implementacion de animal Factory con genericos

---
+++ FactoryMethod/AnimalFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class AnimalFactory
+    {
+        public static Animal CrearAnimal<T>() where T : Animal, new()
+        {
+            return new T();
+        }
+    }
+}
